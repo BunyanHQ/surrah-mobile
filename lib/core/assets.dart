be7b8189f82c9b5a5957550ba0c4790248ef9011
HanyHagar <hanymagdyhagar@gmail.com>
2026-07-26 23:08:44 +0300

    feat(splash): Add Splash Assets

---
+++ lib/core/assets.dart
@@ -0,0 +1,5 @@
+class Assets {
+  static const String appLogo = "assets/images/appLogo.png";
+  static const String splashBg = "assets/images/splashBackground.png";
+  static const String splashLogo = "assets/images/splashLogo.png";
+}
